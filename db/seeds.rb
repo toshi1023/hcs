7346ad08b9c93dc24b095d3107c321bc876a2b2a
toshi <toshimura1992@gmail.com>
2020-02-08 14:08:05 +0900

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 初期データの記述
-# Article.create(id: 1, name: "Yoshida Tsuyoshi", title: "Hello, Everyone", content: "Have a nice day!")
-# Article.create(id: 2, name: "Mickel", title: "Hello, Tsuyoshi", content: "My name is Mickel")
-# Article.create(id: 3, name: "Kaori", title: "Hi, Tsuyoshi! Mickel!", content: "Nice to meet you!")
 
 Member.create(
   id: 1,
