4e974ebe6c2c8927cf0823758f50b000b21921f5
toshi <toshimura1992@gmail.com>
2020-02-09 19:28:49 +0900

    delete step form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,21 +19,3 @@
 //= require jquery.steps
 // jqueryを使用する際は、= require_treeの前にrequire記述を置く
 //= require_tree .
-
-
-// 会員登録のstepフォームを設定
-$().ready(function(){
-	initWizard();
-});
-/** initialize */
-var initWizard = () => {
-	$("#wizard").steps({
-		headerTag: "h1",
-		bodyTag: "section"
-	});
-};
-
-// toggleボタンの動作に必要な設定?
-// $(document).on('turbolinks:render', function(){
-// 	$('input[type="radio"].toggle').bootstrapToggle();
-// });
