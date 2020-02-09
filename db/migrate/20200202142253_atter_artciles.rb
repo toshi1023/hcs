3c1e9a15e129880d7c8fb5d8ec52d0cbafb42282
toshi <toshimura1992@gmail.com>
2020-02-09 20:17:36 +0900

    delete & rename migrate(file)

--- db/migrate/20200202142253_atter_artciles.rb
+++
@@ -1,7 +0,0 @@
-class AtterArtciles < ActiveRecord::Migration[5.2]
-  def change
-    # membersテーブルのIDを外部参照
-    add_reference :articles, :member, foreign_key: true
-    remove_column :articles, :posted_at
-  end
-end
