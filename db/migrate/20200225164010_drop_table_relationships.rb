7abc98bde0303cd52bd66797d777226ad9330006
toshi <toshimura1992@gmail.com>
2020-02-26 01:48:56 +0900

    DB整理

---
+++ db/migrate/20200225164010_drop_table_relationships.rb
@@ -0,0 +1,5 @@
+class DropTableRelationships < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :relationships
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_160334) do
+ActiveRecord::Schema.define(version: 2020_02_25_164702) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
